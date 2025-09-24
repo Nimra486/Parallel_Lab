e5c1ccc6922c63db13c581f25f149073fc6beef4
Nimra <bscs.14.2026@gmail.com>
2025-09-24 08:57:28 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter your name: ");
+        string username = Console.ReadLine();
+
+        for (int i = 5; i >= 0; i--)
+        {
+            Console.WriteLine(i);
+
+            if (i == 5)
+            {
+                Thread.Sleep(500); 
+            }
+            else
+            {
+                Thread.Sleep(1000); 
+            }
+        }
+
+        Console.WriteLine($"Welcome {username}");
+    }
+}
