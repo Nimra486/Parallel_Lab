36a4406c9eb362c67fb8e44d5b06e60c1ce2686f
Nimra <bscs.14.2026@gmail.com>
2025-10-01 10:25:14 +0500

    GENERATE RANDOM NUMBER AND USE MULTITHREADING TO PRINT EVEN AND ODD NUMBER

--- Program.cs
+++ Program.cs
@@ -1,44 +1,25 @@
 ﻿using System;
 using System.Threading;
-
-class Fruits
+class Program
 {
-    string name;
-    int sleeptime;
-
-    public Fruits(string name, int sleeptime)
-    {
-        this.name = name;
-        this.sleeptime = sleeptime;
-    }
-
-    public void Run()
-    {
-        for (int i = 1; i <= 3; i++)
-        {
-            Console.WriteLine($"{DateTime.Now:T} - {name} - Loop {i}");
-            Thread.Sleep(sleeptime * 1000); // Convert seconds to milliseconds
-        }
-    }
-
     static void Main()
     {
-        Fruits apple = new Fruits("Apple", 1);   // Sleep 1 second
-        Fruits mango = new Fruits("Mango", 2);   // Sleep 2 seconds
-        Fruits guava = new Fruits("Guava", 3);   // Sleep 3 seconds
-
-        Thread t1 = new Thread(apple.Run);
-        Thread t2 = new Thread(mango.Run);
-        Thread t3 = new Thread(guava.Run);
-
-        t1.Start();
-        t2.Start();
-        t3.Start();
-
-        t1.Join();
-        t2.Join();
-        t3.Join();
-
-        Console.WriteLine("All Threads finished....");
+        Random number = new Random();
+        int num = number.Next();
+        Console.Write("\nInteger:" + num);
+        if (num % 2 == 0)
+        {
+            num = num * num;
+            Console.Write("\nThe integer is EVEN");
+            Thread.Sleep(1000);
+            Console.Write("\nSquare number=" + num);
+        }
+        if (num % 2 != 0)
+        {
+            num = num * num * num;
+            Console.Write("\nThe integer is ODD");
+            Thread.Sleep(1000);
+            Console.Write("\nCube number=" + num);
+        }
     }
 }
