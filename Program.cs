6a6256516399676bcb26eaa859e39eb75d78c765
Nimra <bscs.14.2026@gmail.com>
2025-10-01 08:57:31 +0500

    Displaying Fruit Names using Threading

--- Program.cs
+++ Program.cs
@@ -1,100 +1,44 @@
 ﻿using System;
 using System.Threading;
-class RainSimulation
+
+class Fruits
 {
+    string name;
+    int sleeptime;
+
+    public Fruits(string name, int sleeptime)
+    {
+        this.name = name;
+        this.sleeptime = sleeptime;
+    }
+
+    public void Run()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"{DateTime.Now:T} - {name} - Loop {i}");
+            Thread.Sleep(sleeptime * 1000); // Convert seconds to milliseconds
+        }
+    }
+
     static void Main()
     {
-        string[] frames = new string[]
-        {
-        @"
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-    |    |    |      |     
- |      |     |    |    |  
-   |   |    |    |     |   
-     |     |      |    |   
-  |    |     |     |    |  
-",
-@"
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-    |    |    |      |     
- |      |     |    |    |  
-   |   |    |    |     |   
-     |     |      |    |   
-",
-@"
-                          
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-    |    |    |      |     
- |      |     |    |    |  
-   |   |    |    |     |   
-",
-@"
-                          
-                          
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-    |    |    |      |     
- |      |     |    |    |  
-",
-@"
-                          
-                          
-                          
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-    |    |    |      |     
-",
-@"
-                          
-                          
-                          
-                          
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
- |     |    |      |    |  
-",
-@"
-                          
-                          
-                          
-                          
-                          
-                          
-  |    |   |  |     |  |   
-    |     |     |   |      
-",
-@"
-                          
-                          
-                          
-                          
-                          
-                          
-                          
-  |    |   |  |     |  |   
-"}; 
-Console.CursorVisible = false;
-for (int i = 0; i < 50; i++)
-{
-    foreach (string frame in frames)
-    {
-        Console.SetCursorPosition(0, 0);
-        Console.WriteLine("\r"+frame);
-        Thread.Sleep(50); } }
-Console.CursorVisible = true;
-}
+        Fruits apple = new Fruits("Apple", 1);   // Sleep 1 second
+        Fruits mango = new Fruits("Mango", 2);   // Sleep 2 seconds
+        Fruits guava = new Fruits("Guava", 3);   // Sleep 3 seconds
 
+        Thread t1 = new Thread(apple.Run);
+        Thread t2 = new Thread(mango.Run);
+        Thread t3 = new Thread(guava.Run);
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+
+        Console.WriteLine("All Threads finished....");
+    }
 }
